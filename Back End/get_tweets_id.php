b986fda7d4b5aaa0f44adfeacb9b7070ddcbfc36
Amine <aminejamal003@gmail.com>
2022-11-04 18:09:10 +0200

    adding register API

---
+++ Back End/get_tweets_id.php
@@ -0,0 +1,27 @@
+<?php 
+include("connection.php");
+
+if(isset($_GET["id"])){
+    $id = $_GET["id"];
+}else{
+    $response = [];
+    $response["success"] = false;   
+    echo json_encode($response);
+    return;  
+}
+
+$query = $mysqli->prepare("Select * from tweets WHERE User_id = ?");
+$query->bind_param("i", $id);
+$query->execute();
+
+$array = $query->get_result();
+$response = [];
+while($tweets = $array->fetch_assoc()){
+    $response[] = $tweets;
+}
+
+if($response[1] == null){
+    die("There are no tweets with this user");
+}
+
+echo json_encode($response);
---
+++ Back End/register.php
@@ -0,0 +1,45 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+include("connection.php");
+
+if(isset($_POST["FName"]) && $_POST["FName"] != "" && isset($_POST["LName"]) && $_POST["LName"] != "" && isset($_POST["Username"]) && $_POST["Username"] != ""&& isset($_POST["Password"]) && $_POST["Password"] != "" ){
+    $FName = $_POST["FName"];
+    $LName = $_POST["LName"];
+    $Username = $_POST["Username"];
+    $Password = $_POST["Password"];
+}else{
+     $response = [];
+     $response["success"] = false;   
+     echo json_encode($response);
+     return; 
+ }
+
+$query = $mysqli->prepare("Select * from users WHERE Username = ?");
+$query->bind_param("s", $Username);
+$query->execute();
+
+$array = $query->get_result();
+
+$response_values = [];
+$response_success = [];
+
+while($users = $array->fetch_assoc()){
+    $response_values[] = $users;
+}
+
+if($response_values[0] == null){
+    $query = $mysqli->prepare("INSERT INTO users( FName, LName, Username, Password) VALUES ( ?, ?, ?, ?)");
+    $query->bind_param("ssss", $FName, $LName, $Username, $Password);
+    $query->execute();
+    $response_success["success"] = true;
+
+    echo json_encode($response);
+}
+
+else{
+    die("User already exist");
+    $response_success["success"] = false;
+}
