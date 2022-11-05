57d15d42ff328f82e00bfd468fbe3cdf44525ddb
Chloe Gemayel <chloegemayel@Chloes-MacBook-Pro.local>
2022-11-05 13:17:21 +0200

    Adding splash screen

--- Front End/app/src/main/java/com/example/retweet/MainActivity.java
+++ Front End/app/src/main/java/com/example/retweet/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.retweet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+/*
+    Activity 1 --> Splash Screen which will show for 2 seconds
+                    and go to the next activity which is the
+                    login page.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread thread = new Thread() {
+
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(MainActivity.this, MainActivity1.class));
+                }
+            }
+
+        };
+        thread.start();
     }
 }
