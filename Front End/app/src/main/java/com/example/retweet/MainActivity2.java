ccfbaafe3c52715e2e35ca5e8007e27d897a4742
Chloe Gemayel <chloegemayel@Chloes-MacBook-Pro.local>
2022-11-05 17:41:56 +0200

    Desiging sign up template

---
+++ Front End/app/src/main/java/com/example/retweet/MainActivity2.java
@@ -0,0 +1,23 @@
+package com.example.retweet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/*
+    Activity 3 --> We have now moved to the register page
+                    for a new user to create a new account
+                    by filling in their fname, lname, and
+                    username and password.
+ */
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void getStarted(){
+
+    }
+}
