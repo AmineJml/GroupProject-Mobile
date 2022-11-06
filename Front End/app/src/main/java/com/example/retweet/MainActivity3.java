4b6630f928f947e122ab5d0cd5bfd69a8d2d4f03
Chloe Gemayel <chloegemayel@Chloes-MacBook-Pro.local>
2022-11-06 14:32:57 +0200

    Adding Volley

--- Front End/app/src/main/java/com/example/retweet/MainActivity3.java
+++ Front End/app/src/main/java/com/example/retweet/MainActivity3.java
@@ -3,16 +3,25 @@ package com.example.retweet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity3 extends AppCompatActivity {
 
-    //view all tweets.php
-    String base_url = "http://192.168.0.108./view_all_tweets.php";
+    String base_url = "http://192.168.0.108.";
+    RequestQueue queue;
+    StringRequest request;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        queue = Volley.newRequestQueue(MainActivity3.this);
     }
+
+
+
 
 
 }
