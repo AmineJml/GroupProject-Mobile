5a7e00b7365c02474a0fe4e5c24ebe63b0cabee8
Chloe Gemayel <chloegemayel@Chloes-MacBook-Pro.local>
2022-11-06 13:41:26 +0200

    Fixing feed view

--- Front End/app/src/main/java/com/example/retweet/MainActivity3.java
+++ Front End/app/src/main/java/com/example/retweet/MainActivity3.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 
 public class MainActivity3 extends AppCompatActivity {
 
+    //view all tweets.php
+    String base_url = "http://192.168.0.108./view_all_tweets.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
     }
+
+
 }
