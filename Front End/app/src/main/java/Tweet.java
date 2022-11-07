3abff4e572ebe65c6bc59b827a3dd81bd6c6eefd
Chloe Gemayel <chloegemayel@Chloes-MacBook-Pro.local>
2022-11-07 12:23:40 +0200

    Created tweet class to use in RecycleView

---
+++ Front End/app/src/main/java/Tweet.java
@@ -0,0 +1,26 @@
+
+public class Tweet {
+    String username;
+    String tweet_content;
+
+    public Tweet(String username, String tweet_content){
+        this.username=username;
+        this.tweet_content=tweet_content;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTweet_content() {
+        return tweet_content;
+    }
+
+    public void setTweet_content(String tweet_content) {
+        this.tweet_content = tweet_content;
+    }
+}
